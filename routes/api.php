76be2639341e105506583c50e73dcd43d3e52b62
Abdul Haseeb <haseeb@vdigitalx.com>
2024-11-08 12:52:07 +0500

    Create a API routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotesController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LabelController;
 
 
 /*
@@ -23,11 +24,22 @@ Route::get('/', function () {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/notes', [NotesController::class, 'index']);
+    Route::get('/search', [NotesController::class, 'search']);
     Route::post('/notes', [NotesController::class, 'store']);
     Route::get('/notes/{id}', [NotesController::class, 'show']);
     Route::put('/notes/{id}', [NotesController::class, 'update']);
     Route::delete('/notes/{id}', [NotesController::class, 'destroy']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/labels', [LabelController::class, 'index']);
+    Route::post('/labels', [LabelController::class, 'store']);
+    Route::get('/labels/{id}', [LabelController::class, 'show']);
+    Route::put('/labels/{id}', [LabelController::class, 'update']);
+    Route::delete('/labels/{id}', [LabelController::class, 'destroy']);
 });
