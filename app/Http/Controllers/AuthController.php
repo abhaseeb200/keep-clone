243b97590f3f6f24824b7deafdf2af70e61e14fe
Abdul Haseeb <haseeb@vdigitalx.com>
2024-11-08 12:48:20 +0500

    Create a forgotPassword API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,10 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Password;
+// use Illuminate\Support\Str;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Mail;
 
 class AuthController extends Controller
 {
@@ -60,5 +64,50 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'Bearer',
         ]);
+    }
+
+    public function forgotPassword(Request $request) {
+        $request->validate(['email' => 'required|email']);
+ 
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json(['message' => 'Email not found.'], 404);
+        }
+
+        $status = Password::sendResetLink($request->only('email'));
+        if ($status === Password::RESET_LINK_SENT) {
+            return response()->json(['message' => 'Password reset link sent to your email.'], 200);
+        }
+        
+        return response()->json(['message' => __($status)], 500);
+    }
+
+    public function resetPassword(Request $request) {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|string|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+
+            function (User $user, string $password) {
+                // $user->forceFill([
+                //     'password' => Hash::make($password)
+                // ])->setRememberToken(Str::random(60));
+                $user->password = Hash::make($password);
+                $user->save();
+                // event(new PasswordReset($user));
+            }
+        );
+
+        if ($status === Password::PASSWORD_RESET) {
+            // return response()->json(['message' => __($status)], 200);
+            return response()->json(['message' => 'Password has been reset successfully.'], 200);
+        }
+
+        return response()->json(['message' => 'Failed to reset password.'], 500);
+        // response()->json(['error' => __($status)], 422);
     }
 }
