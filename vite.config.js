b99f50fc5f1a94379da760fef96813a3793e4f0a
Abdul Haseeb <haseeb@vdigitalx.com>
2024-11-08 12:44:58 +0500

    Integrated tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/sass/app.scss", "resources/js/app.jsx"],
+            input: ["resources/js/app.js", "resources/css/app.css"],
             refresh: true,
         }),
         react(),
     ],
-    // plugins: [
-    //     laravel(['resources/js/app.jsx']),
-    //     react(),
-    // ],
+
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+        },
+    },
 });
