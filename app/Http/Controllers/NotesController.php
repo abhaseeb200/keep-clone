b5b92d351d82d8e559a3165ab9923e5617d2b3aa
Abdul Haseeb <haseeb@vdigitalx.com>
2024-11-08 12:49:21 +0500

    Modified the Notes to join with Labels data

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -5,58 +5,137 @@ namespace App\Http\Controllers;
 use App\Models\Note;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 
 class NotesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $notes = Auth::user()->notes()->get();
-        return response()->json($notes);
+        $sortBy = $request->get('sort_by', 'created_at'); 
+        $order = $request->get('order', 'asc');
+        $limit = $request->get('limit', 5);
+    
+        if (!in_array($sortBy, ['title', 'created_at', 'content'])) {
+            return response()->json(['message' => 'Invalid sort field.'], 400);
+        }
+        if (!in_array($order, ['asc', 'desc'])) {
+            return response()->json(['message' => 'Invalid order.'], 400);
+        }
+    
+        $data = Auth::user()->notes()->with("labels")->orderBy($sortBy, $order)->paginate($limit);  
+        return response()->json([
+            'count' => $data->count(),
+            'data' => $data
+        ]);
+    }
+
+    public function search(Request $request)
+    {
+        $limit = $request->get('limit', 5);
+        $request->validate([
+            'query' => 'required|string|max:255',
+        ]);
+
+        $query = $request->input('query');
+
+        $results = Auth::user()->notes()
+        ->where('title', 'like', "%{$query}%")
+        ->orWhere('content', 'like', "%{$query}%")->paginate($limit);
+
+        return response()->json([
+            'count' => $results->count(),
+            'data' => $results
+        ]);
     }
 
     // Store a newly created post in storage
     public function store(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'content' => 'required|string',
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'isPinned' => 'nullable|boolean',
+            'isArchived' => 'nullable|boolean',
+            'background_color' => 'nullable|string',
+            'labels' => 'array',
+            'labels.*' => 'exists:labels,id' //Used to ensure that each label ID provided in the labels
         ]);
 
         if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $imagePath = $image->store('images', 'public');
+            $imageName = time().'.'.$request->image->extension();
+            $request->image->move(public_path('images'), $imageName);
+            $imagePath = 'images/'.$imageName;
+            // $image = $request->file('image');
+            // $imagePath = $image->store('images', 'public');
         }
         
         $note = Auth::user()->notes()->create([
             'title' => $request->input('title'),
             'content' => $request->input('content'),
-            'image' => $imagePath,
+            'image' => $imagePath ?? "",
+            'isPinned' => $request->input('isPinned', false),
+            'isArchived' => $request->input('isArchived', false),
+            'background_color' => $request->input('background_color', '')
         ]);
 
+         // Attach labels if provided
+         if (isset($validatedData['labels'])) {
+            $note->labels()->attach($validatedData['labels']);
+        }
+
         // $note = Auth::user()->notes()->create($request->all());
-        return response()->json(['message' => 'Added successfully', 'data' => $note]);
+        // return response()->json(['message' => 'Added successfully', 'data' => $note]);
+        return response()->json(['message' => 'Added successfully', 'data' => $note->load('labels')]);
     }
 
     // Display the specified post
     public function show($id)
     {
-        $note = Auth::user()->notes()->findOrFail($id);
-        return response()->json($note);
+        $note = Auth::user()->notes()->with('labels')->findOrFail($id);
+        return response()->json(['data' => $note]);
     }
 
     // Update the specified post in storage
     public function update(Request $request, $id)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'title' => 'required|string|max:255',
             'content' => 'required|string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'isPinned' => 'nullable|boolean',
+            'isArchived' => 'nullable|boolean',
+            'background_color' => 'nullable|string',
+            'labels' => 'array',
+            'labels.*' => 'exists:labels,id'
         ]);
 
         $note = Auth::user()->notes()->findOrFail($id);
-        $note->update($request->all());
-        return response()->json(['message' => 'Post updated successfully', 'post' => $note]);
+
+        if ($request->hasFile('image')) {
+            $imageName = time().'.'.$request->image->extension();
+            $request->image->move(public_path('images'), $imageName);
+            $imagePath = 'images/'.$imageName;
+            $note->update(["image" => $imagePath]);
+        }
+
+        // $note = Auth::user()->notes()->findOrFail($id)->update($request->all()); 
+        $note->update([
+            'title' => $request->input('title'), 
+            'content' => $request->input('content'),
+            'image' => $imagePath ?? "",
+            'isPinned' => $request->input('isPinned'),
+            'isArchived' => $request->input('isArchived'),
+            'background_color' => $request->input('background_color')
+        ]);
+
+        // Sync labels if provided
+        if (isset($validatedData['labels'])) {
+            $note->labels()->sync($validatedData['labels']);
+        }
+
+        return response()->json(['message' => 'Updated successfully', 'post' => $note->load('labels')]);
     }
 
     // Remove the specified post from storage
