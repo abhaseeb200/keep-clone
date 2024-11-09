af2bce19277aa1787cf640ed22291b06c08ab095
Abdul Haseeb <haseebabdul747@gmail.com>
2024-11-09 23:59:49 +0500

    Add protected routes

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignIn from "./screens/sign-in";
-import SignUp from "./screens/sign-up";
-import Dashboard from "./screens/dashboard";
+import SignIn from "@/Screens/sign-in";
+import SignUp from "@/Screens/sign-up";
+import ForgotPassword from "@/Screens/forgot-password";
+import Dashboard from "@/Screens/dashboard";
+import User from "@/Screens/user";
+import AuthLayout from "@/Layouts/AuthLayout";
+import MainLayout from "@/Layouts/MainLayout";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<SignIn />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                {/* =============== PUBLIC ROUTES =============== */}
+                <Route element={<AuthLayout />}>
+                    <Route path="/" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route
+                        path="/forgot-password"
+                        element={<ForgotPassword />}
+                    />
+                </Route>
+
+                {/* =============== PROTECTED ROUTES =============== */}
+                <Route element={<MainLayout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/users" element={<User />} />
+                </Route>
             </Routes>
         </Router>
     );
