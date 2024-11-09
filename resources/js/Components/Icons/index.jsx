588c52e5cacf8c97bbbd9e718ed4ef4563752a19
Abdul Haseeb <haseebabdul747@gmail.com>
2024-11-10 00:01:12 +0500

    Add Svgs Icons

---
+++ resources/js/Components/Icons/index.jsx
@@ -0,0 +1,124 @@
+export const ImageIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="#000"
+        >
+            <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zm-5-7l-3 3.72L9 13l-3 4h12l-4-5z" />
+        </svg>
+    );
+};
+
+export const PinIcon = ({ className }) => {
+    return (
+        <svg
+            className={className}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+        >
+            <path fill="none" d="M0 0h24v24H0z" />
+            <path
+                fill="#000"
+                d="M17 4v7l2 3v2h-6v5l-1 1-1-1v-5H5v-2l2-3V4c0-1.1.9-2 2-2h6c1.11 0 2 .89 2 2zM9 4v7.75L7.5 14h9L15 11.75V4H9z"
+            />
+        </svg>
+    );
+};
+
+export const ColorIcon = ({ className }) => {
+    return (
+        <svg
+            className={className}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="#000"
+        >
+            <path d="M12 22C6.49 22 2 17.51 2 12S6.49 2 12 2s10 4.04 10 9c0 3.31-2.69 6-6 6h-1.77c-.28 0-.5.22-.5.5 0 .12.05.23.13.33.41.47.64 1.06.64 1.67A2.5 2.5 0 0 1 12 22zm0-18c-4.41 0-8 3.59-8 8s3.59 8 8 8c.28 0 .5-.22.5-.5a.54.54 0 0 0-.14-.35c-.41-.46-.63-1.05-.63-1.65a2.5 2.5 0 0 1 2.5-2.5H16c2.21 0 4-1.79 4-4 0-3.86-3.59-7-8-7z" />
+            <circle cx="6.5" cy="11.5" r="1.5" />
+            <circle cx="9.5" cy="7.5" r="1.5" />
+            <circle cx="14.5" cy="7.5" r="1.5" />
+            <circle cx="17.5" cy="11.5" r="1.5" />
+        </svg>
+    );
+};
+
+export const ArchivedIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="#000"
+        >
+            <path d="M20.54 5.23l-1.39-1.68C18.88 3.21 18.47 3 18 3H6c-.47 0-.88.21-1.16.55L3.46 5.23C3.17 5.57 3 6.02 3 6.5V19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V6.5c0-.48-.17-.93-.46-1.27zM6.24 5h11.52l.83 1H5.42l.82-1zM5 19V8h14v11H5zm11-5.5l-4 4-4-4 1.41-1.41L11 13.67V10h2v3.67l1.59-1.59L16 13.5z" />
+        </svg>
+    );
+};
+
+export const LabelIcon = ({ className }) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+        >
+            <path d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16zM16 17H5V7h11l3.55 5L16 17z"></path>
+        </svg>
+    );
+};
+
+export const TickIcon = ({ className }) => {
+    return (
+        <svg
+            className={className}
+            width="24px"
+            height="24px"
+            viewBox="0 0 24 24"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+            <defs>
+                <path
+                    d="M12,2 C17.52,2 22,6.48 22,12 C22,17.52 17.52,22 12,22 C6.48,22 2,17.52 2,12 C2,6.48 6.48,2 12,2 Z M10,14.2 L7.4,11.6 L6,13 L10,17 L18,9 L16.6,7.6 L10,14.2 Z"
+                    id="path-1"
+                />
+            </defs>
+            <g stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                <polygon points="0 0 24 0 24 24 0 24" />
+                <mask fill="white">
+                    <use xlinkHref="#path-1" />
+                </mask>
+                <use fill="#000000" fillRule="nonzero" xlinkHref="#path-1" />
+            </g>
+        </svg>
+    );
+};
+
+export const CrossIcon = ({ className }) => {
+    return (
+        <svg
+            className={className}
+            height="18px"
+            width="18px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 18 18"
+            fill="#000000"
+        >
+            <path d="m0 0h18v18h-18zh18v18h-18z" fill="none" />
+            <path d="m14.53 4.53l-1.06-1.06-4.47 4.47-4.47-4.47-1.06 1.06 4.47 4.47-4.47 4.47 1.06 1.06 4.47-4.47 4.47 4.47 1.06-1.06-4.47-4.47z" />
+        </svg>
+    );
+};
