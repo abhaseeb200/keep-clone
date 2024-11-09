9d1ca6e6fae89ddaf443076a7e0d4cde90d28e13
Abdul Haseeb <haseebabdul747@gmail.com>
2024-11-10 00:00:57 +0500

    Modified the button styling

--- resources/js/Components/Button/index.jsx
+++ resources/js/Components/Button/index.jsx
@@ -4,7 +4,7 @@ function Button({ title, type, className, onClick = () => {} }) {
     return (
         <button
             onClick={onClick}
-            className={`bg-black text-white uppercase text-sm flex justify-center items-center py-3 px-5 rounded-md transition-all duration-400 ease-in-out hover:bg-gray-800 ${className}`}
+            className={`bg-black text-white uppercase text-sm flex justify-center items-center h-[50px] px-5 rounded-md transition-all duration-400 ease-in-out hover:bg-gray-800 ${className}`}
             type={type}
         >
             {title}
