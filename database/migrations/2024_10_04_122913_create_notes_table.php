1e5bdfee394832a128cdf846f9b9f063a48db5eb
Abdul Haseeb <haseebabdul747@gmail.com>
2024-11-17 14:50:04 +0500

    Rename column background_color to background

--- database/migrations/2024_10_04_122913_create_notes_table.php
+++ database/migrations/2024_10_04_122913_create_notes_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
+            $table->string('image');
             $table->text('content');
             $table->boolean('isPinned')->default(false);
             $table->boolean('isArchived')->default(false);
-            $table->string('background_color');
+            $table->string('background');
             $table->timestamps();
         });
     }
