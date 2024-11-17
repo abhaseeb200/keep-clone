ea58ec8fcbc5afcedfb1978c7ef847c8a8fde6b5
Abdul Haseeb <haseebabdul747@gmail.com>
2024-11-17 14:53:40 +0500

    Add new icons, and dynamic it

--- resources/js/Components/Icons/index.jsx
+++ resources/js/Components/Icons/index.jsx
@@ -13,9 +13,10 @@ export const ImageIcon = ({ className }) => {
     );
 };
 
-export const PinIcon = ({ className }) => {
+export const PinIcon = ({ className, onClick }) => {
     return (
         <svg
+            onClick={onClick}
             className={className}
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -50,9 +51,10 @@ export const ColorIcon = ({ className }) => {
     );
 };
 
-export const ArchivedIcon = ({ className }) => {
+export const ArchivedIcon = ({ className, onClick }) => {
     return (
         <svg
+            onClick={onClick}
             xmlns="http://www.w3.org/2000/svg"
             className={className}
             width="24"
@@ -75,6 +77,23 @@ export const LabelIcon = ({ className }) => {
             viewBox="0 0 24 24"
         >
             <path d="M17.63 5.84C17.27 5.33 16.67 5 16 5L5 5.01C3.9 5.01 3 5.9 3 7v10c0 1.1.9 1.99 2 1.99L16 19c.67 0 1.27-.33 1.63-.84L22 12l-4.37-6.16zM16 17H5V7h11l3.55 5L16 17z"></path>
+        </svg>
+    );
+};
+
+export const LabelFillIcon = ({ onClick, className }) => {
+    return (
+        <svg
+            className={className}
+            onClick={onClick}
+            height="18px"
+            width="18px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            fill="#000000"
+        >
+            <path d="m0 0h48v48h-48z" fill="none" />
+            <path d="m35.27 11.69c-0.73-1.02-1.92-1.69-3.27-1.69l-22 0.02c-2.21 0-4 1.77-4 3.98v20c0 2.21 1.79 3.98 4 3.98l22 0.02c1.35 0 2.54-0.67 3.27-1.69l8.73-12.31-8.73-12.31z" />
         </svg>
     );
 };
@@ -108,6 +127,21 @@ export const TickIcon = ({ className, onClick }) => {
     );
 };
 
+export const TickWithoutBackgoundIcon = ({ className, onClick }) => {
+    return (
+        <svg
+            height="18px"
+            width="18px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 18 18"
+            fill="#000000"
+        >
+            <path d="m0 0h18v18h-18z" fill="none" />
+            <path d="m6.61 11.89l-3.11-3.11-1.06 1.06 4.17 4.16 8.95-8.95-1.06-1.05z" />
+        </svg>
+    );
+};
+
 export const CrossIcon = ({ className, onClick }) => {
     return (
         <svg
@@ -130,15 +164,92 @@ export const TrashIcon = ({ onClick, className }) => {
         <svg
             className={className}
             onClick={onClick}
-            width="30px"
-            height="30px"
-            viewBox="0 0 1024 1024"
             xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
         >
-            <path
-                fill="#000000"
-                d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"
-            />
+            <path d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13z"></path>
+            <path d="M9 8h2v9H9zm4 0h2v9h-2z"></path>
+        </svg>
+    );
+};
+
+export const TrashFillIcon = ({ onClick, className }) => {
+    return (
+        <svg
+            className={className}
+            onClick={onClick}
+            height="18px"
+            width="18px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            fill="#000000"
+        >
+            <path d="m12 38c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4v-24h-24v24zm26-30h-7l-2-2h-10l-2 2h-7v4h28v-4z" />
+            <path d="m0 0h48v48h-48z" fill="none" />
+        </svg>
+    );
+};
+
+export const BulbIcon = ({ className }) => {
+    return (
+        <svg
+            className={className}
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+        >
+            <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6A4.997 4.997 0 0 1 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
+        </svg>
+    );
+};
+
+export const PencilIcon = ({ className }) => {
+    return (
+        <svg
+            className={className}
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+        >
+            <path d="M20.41 4.94l-1.35-1.35c-.78-.78-2.05-.78-2.83 0L13.4 6.41 3 16.82V21h4.18l10.46-10.46 2.77-2.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 1.41 1.41-9.82 9.83z" />
+        </svg>
+    );
+};
+
+export const PencilFillIcon = ({ className, onClick }) => {
+    return (
+        <svg
+            className={className}
+            onClick={onClick}
+            height="18px"
+            width="18px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            fill="#000000"
+        >
+            <path d="m6 34.5v7.5h7.5l22.13-22.13-7.5-7.5-22.13 22.13zm35.41-20.41c0.78-0.78 0.78-2.05 0-2.83l-4.67-4.67c-0.78-0.78-2.05-0.78-2.83 0l-3.66 3.66 7.5 7.5 3.66-3.66z" />
+            <path d="m0 0h48v48h-48z" fill="none" />
+        </svg>
+    );
+};
+
+export const PlusIcon = ({ className, onClick }) => {
+    return (
+        <svg
+            className={className}
+            onClick={onClick}
+            height="18px"
+            width="18px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            fill="#000000"
+        >
+            <path d="m38 26h-12v12h-4v-12h-12v-4h12v-12h4v12h12v4z" />
+            <path d="m0 0h48v48h-48z" fill="none" />
         </svg>
     );
 };
