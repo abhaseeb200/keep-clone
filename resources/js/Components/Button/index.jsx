8e1ae29bc949c6324424eadf006d9f7fa8524d01
Abdul Haseeb <haseebabdul747@gmail.com>
2024-11-17 15:30:21 +0500

    Add spinner icon to button

--- resources/js/Components/Button/index.jsx
+++ resources/js/Components/Button/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { SpinnerIcon } from "@/Components/Icons";
 
-function Button({ title, type, className, onClick = () => {} }) {
+function Button({ title, type, className, isLoading, onClick = () => {} }) {
     return (
         <button
+            disabled={isLoading}
             onClick={onClick}
-            className={`bg-black text-white uppercase text-sm flex justify-center items-center h-[50px] px-5 rounded-md transition-all duration-400 ease-in-out hover:bg-gray-800 ${className}`}
+            className={`bg-black gap-2 text-white uppercase text-sm flex justify-center items-center h-[50px] px-5 rounded-md transition-all duration-400 ease-in-out hover:bg-gray-800 ${className}`}
             type={type}
         >
-            {title}
+            {isLoading && <SpinnerIcon className="size-4" />} {title}
         </button>
     );
 }
