fe9348f3b496fbd0e485f46bc88b63f097fc8bc9
Abdul Haseeb <haseebabdul747@gmail.com>
2024-11-17 14:50:48 +0500

    Add toaster, LabelModal

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import { store, persistor } from "./Config/store.js";
 import AuthLayout from "@/Layouts/AuthLayout";
 import MainLayout from "@/Layouts/MainLayout";
+import LabelModal from "@/Components/LabelModal";
 import SignIn from "@/Screens/sign-in";
 import SignUp from "@/Screens/sign-up";
 import ForgotPassword from "@/Screens/forgot-password";
@@ -13,26 +15,34 @@ import Dashboard from "@/Screens/dashboard";
 import User from "@/Screens/user";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                {/* =============== PUBLIC ROUTES =============== */}
-                <Route element={<AuthLayout />}>
-                    <Route path="/" element={<SignIn />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route
-                        path="/forgot-password"
-                        element={<ForgotPassword />}
-                    />
-                </Route>
+    const [isOpen, setIsOpen] = useState(false);
 
-                {/* =============== PROTECTED ROUTES =============== */}
-                <Route element={<MainLayout />}>
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/users" element={<User />} />
-                </Route>
-            </Routes>
-        </Router>
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+    return (
+        <>
+            <Toaster position="top-center" reverseOrder={false} />
+            <LabelModal closeModal={closeModal} isOpen={isOpen} />
+            <Router>
+                <Routes>
+                    {/* =============== PUBLIC ROUTES =============== */}
+                    <Route element={<AuthLayout />}>
+                        <Route path="/" element={<SignIn />} />
+                        <Route path="/signup" element={<SignUp />} />
+                        <Route
+                            path="/forgot-password"
+                            element={<ForgotPassword />}
+                        />
+                    </Route>
+
+                    {/* =============== PROTECTED ROUTES =============== */}
+                    <Route element={<MainLayout openModal={openModal} />}>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/users" element={<User />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </>
     );
 }
 
